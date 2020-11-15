e5980afc5bd58c7ec1d8a02300866517035ac7ed
Cyle Ven Dawson <dawsonc623@gmail.com>
2020-11-15 15:44:02 -0600

    fix: fix casing on prototype 1 scene loading

--- screens/title/title_screen.gd
+++ screens/title/title_screen.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 
 func _on_prototype1_pressed():
-	var _result = get_tree().change_scene("res://screens/Prototype1/Game.tscn")
+	var _result = get_tree().change_scene("res://screens/prototype1/game.tscn")
