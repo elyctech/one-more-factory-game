3106da6c824608678643f2bdd333c506e490b0dc
Cyle Ven Dawson <dawsonc623@gmail.com>
2020-11-16 11:12:40 -0600

    refactor: fix case-insensitivity

--- Components/in_game_menu/in_game_menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-var menu_container;
-
-var controls_menu : Control = preload("res://Components/InGameMenu/Controls/Controls.tscn").instance()
-
-
-func _on_controls_pressed():
-	menu_container.change_menu(controls_menu)
