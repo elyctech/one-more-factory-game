6eb16c2f661e399acf7ed75ff628cedc55650896
Cyle Ven Dawson <dawsonc623@gmail.com>
2020-11-16 08:48:19 -0600

    refactor: change resource name algorithm to a look-up

--- Components/resource_layer/resource_layer.gd
+++ Components/resource_layer/resource_layer.gd
@@ -3,6 +3,19 @@ extends Node2D
 
 var _resource_tiles : Array
 
+# TODO Do not hard-code this?
+var _resource_names := {
+	"coal1" : "Coal",
+	"coal2" : "Coal",
+	"coal3" : "Coal",
+	"copper1" : "Copper Ore",
+	"copper2" : "Copper Ore",
+	"copper3" : "Copper Ore",
+	"iron1" : "Iron Ore",
+	"iron2" : "Iron Ore",
+	"iron3" : "Iron Ore"
+}
+
 onready var _resource_tilemap : TileMap = get_node("Resources")
 
 
@@ -15,18 +28,18 @@ func get_resource_cell_size():
 
 
 func get_resource_name_at(cell_x, cell_y):
-	# Tiles are named "<resource><version>", so get the resource name from the
-	# tile at the desired location and capitalize it
-	var resource_name = _resource_tilemap.tile_set.tile_get_name(
+	var tile_name = _resource_tilemap.tile_set.tile_get_name(
 		_resource_tilemap.get_cell(
 			cell_x,
 			cell_y
 		)
-	).capitalize()
+	)
 	
-	# Strip the number. Currently, there are only single digits, so we assume
-	# we can strip just the last character.
-	resource_name = resource_name.substr(0, resource_name.length() - 1)
+	var resource_name = ""
+	
+	# TODO Should not having this tile be an error or warning?
+	if _resource_names.has(tile_name):
+		resource_name = _resource_names[tile_name]
 	
 	return resource_name
 
