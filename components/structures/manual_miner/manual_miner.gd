69c03bc7c7daaa36e3fbd5cea16d839a06cb27e9
Cyle Ven Dawson <dawsonc623@gmail.com>
2020-11-19 09:59:12 -0600

    feat: limit mining rate on manual miners

--- components/structures/manual_miner/manual_miner.gd
+++ components/structures/manual_miner/manual_miner.gd
@@ -4,9 +4,23 @@ extends Area2D
 var item_name := "Manual Miner"
 var resource_name := ""
 
+var _interaction_response := preload("res://ui/interaction_response/interaction_response.tscn")
+var _last_mining := 0
+var _mining_rate := 500
+
 onready var warehouse = get_node("/root/warehouse")
 
 
 func _on_input_event(_viewport, event, _shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT and resource_name != "":
-		warehouse.add_item(resource_name)
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		if resource_name != "" and OS.get_ticks_msec() - _last_mining >= _mining_rate:
+			warehouse.add_item(resource_name)
+			
+			var mining_response = _interaction_response.instance()
+			mining_response.text = "+1 %s" % resource_name
+					
+			self.add_child(
+				mining_response
+			)
+			
+			_last_mining = OS.get_ticks_msec()
